7a9f6f1f10db41a1309f78cb3663dcb5af75f171
Abhishek Soni <abhisheksoni1998@gmail.com>
2026-01-28 17:31:07 +0530

    Readme file for WishlistApp and minor fixes

---
+++ WishlistApp/Readme.md
@@ -0,0 +1,19 @@
+# Quiz CLI App
+A beginner-friendly app that demonstrates core **JavaScript skills**
+
+---
+
+## ⚙️ Installation & Setup
+- Prerequesites: VS Code, NodeJS, Live Server Extension
+- 🛠️ Steps to Run CLI App
+    - Open Folder in CMD -> WishlistApp
+    - Install Packages -> npm i
+    - Run App -> Open index.html file with Live Server
+
+---
+
+## Author
+
+Abhishek Soni
+
+Email: abhisheksoni1998@gmail.com
--- WishlistApp/index.js
+++ WishlistApp/index.js
@@ -30,10 +30,10 @@ function renderTodoList() {
     let iH = ''
     todoList.forEach(({id, todo, isCompleted}) => {
         iH +=  `
-            <li class="list-group-item" data-key=${id}>
-                <input class="form-check-input mt-2" id="item-${id}" type="checkbox"  ${isCompleted ? "checked": ""}>
+            <li class="list-group-item ${isCompleted ? "bg-success" : "bg-danger"} mt-2" data-key=${id}>
+                <input class="form-check-input mt-2" id="item-${id}" type="hidden" data-key=${id}>
                     <label for="item-${id}" class="todo todo-text t-pointer ${isCompleted ? "checked-todo" : ""}" data-key=${id}> &nbsp; ${todo}</label>
-                    <button type="button" class="btn btn-dark" data-todokey=${id}>
+                    <button type="button" class="btn ${isCompleted ? "btn-success" : "btn-danger"}" data-todokey=${id}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" data-todokey=${id}>
                             <path data-todokey=${id} d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                             <path data-todokey=${id} d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
